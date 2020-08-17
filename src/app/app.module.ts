911ef0b5d4fbd1eb30a75a95b7e1e7b0d4d3cfc1
gerchay <2995058550101@ingenieria.usac.edu.gt>
2020-08-17 17:20:48 -0600

    Listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+// FIREBASE
+import { AngularFireModule } from 'angularfire2'; // BConectarse a los servicios de firebase
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+// ENVIRONMENT Config
+import { environment } from '../environments/environment';
+//COMPONENTS
+import { TodoComponent } from './components/todo/todo.component';
+// SERVICES
+import { TodoService }  from './services/todo.service'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [
+    TodoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAadvW0ZC2MWrI6DqRAtI215l6G6qxdb_w",
+    authDomain: "taskmanager-dd53b.firebaseapp.com",
+    databaseURL: "https://taskmanager-dd53b.firebaseio.com",
+    projectId: "taskmanager-dd53b",
+    storageBucket: "taskmanager-dd53b.appspot.com",
+    messagingSenderId: "317512375497",
+    appId: "1:317512375497:web:cdf21c76db3f30e2224118",
+    measurementId: "G-DD4X85RRDR"
+  }
 };
 
 /*
